96cdba358a12b0d584f86c5fb5f14550a2c13efa
kendall529 <kendall.smith529@gmail.com>
2023-06-16 08:57:37 -0500

    can now insert employees, roles, and departments

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const inquirer = require('inquirer');
 
 const db = require('./db/connection');
 
+// const consTable = require('console.table');
+
 const init = () => {
     inquirer
         .prompt([
@@ -23,10 +25,18 @@ const init = () => {
             }
         ])
         .then(res => {
-            switch (res.start) {
+            switch (res.init) {
                 case 'View all departments': viewDepartments();
                     break;
                 case 'View all roles': viewRoles();
+                    break;
+                case 'View all employees': viewEmployees();
+                    break;
+                case 'Add a department': insertDepartment();
+                    break;
+                case 'Add a role': insertRole();
+                    break;
+                case 'Add an employee': insertEmployee();
             }
         })
         .catch(err => console.error(err));
@@ -50,4 +60,110 @@ const viewRoles = () => {
         console.table(results);
         init();
     });
-}
+};
+
+const viewEmployees = () => {
+    db.query(`SELECT * FROM employees`, (err, results) => {
+        if(err) throw err;
+
+        console.table(results);
+        init();
+    });
+};
+
+const insertDepartment = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'Enter the name of the department.',
+                name: 'addDepartment',
+            }
+        ])
+        .then(res => {
+            db.query(`INSERT INTO departments(name)
+                      VALUES(?)`, res.addDepartment, (err, results) => {
+                        if(err) throw err;
+
+                        db.query(`SELECT * FROM departments`, (err, results) => {
+                            if(err) throw err;
+                            init();
+                        })
+                      })
+        })
+};
+
+const insertRole = () => {
+    const departmentChoices = () => {
+        db.promise()
+        .query(`SELECT * FROM department`)
+        .then((rows) => {
+            let arrNames = rows[0].map(obj => obj.name);
+
+            return arrNames;
+        })
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    message: 'Enter the name of the job title.',
+                    name: 'roleTitle',
+                },
+                {
+                    type: 'input',
+                    message: 'Enter the salary.',
+                    name: 'roleSalary',
+                },
+                {
+                    type: 'list',
+                    message: 'Select the department this role belongs to.',
+                    name: 'roleDepartment',
+                    choices: departmentChoices,
+                }
+            ])
+            .then(res => {
+                db.promise()
+                .query(`SELECT id FROM department WHERE name = ?`, res.ad)
+                .then(response => {
+                    let arrID = response[0].map(obj => obj.id);
+
+                    return arrID[0];
+                })
+                .then(arrID => {
+                    db.promise()
+                    .query(`INSERT INTO roles(title, salary, department_id)
+                            VALUES(?, ?, ?)`, [res.roleTitle, res.roleSalary, res.roleDepartment]);
+                    init();
+                });
+            });
+    };
+};
+
+const insertEmployee = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: `Enter employee's first name.`,
+                name: 'firstName',
+            },
+            {
+                type: 'input',
+                message: `Enter employee's last name.`,
+                name: 'lastName',
+            },
+        ])
+        .then(res => {
+            db.query(`INSERT INTO employees(first_name, last_name)
+                      VALUES(?, ?)`, [res.firstName, res.lastName], (err, results) => {
+                        if(err) throw err;
+
+                        db.query(`SELECT * FROM employees`, (err, results) => {
+                            if(err) throw err;
+
+                            console.table(results);
+                            init();
+                        });
+                      });
+        });
+};
